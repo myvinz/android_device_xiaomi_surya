a3a58762e2b404c60d2456e6162b828f842c37b3
myvinz <134037310+myvinz@users.noreply.github.com>
2024-02-20 23:54:03 +0700

    Update and rename lineage_surya.mk to rising_surya.mk

--- rising_surya.mk
+++ rising_surya.mk
@@ -11,10 +11,20 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from surya device
 $(call inherit-product, device/xiaomi/surya/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common RisingOS stuff.
+$(call inherit-product, vendor/rising/config/rising.mk)
 
-PRODUCT_NAME := lineage_surya
+# RisingOs Flags
+RISING_CHIPSET := SDM732G
+RISING_MAINTAINER := Skyy | 私は空です
+WITH_GMS := false
+TARGET_CORE_GMS := true
+TARGET_ENABLE_BLUR := true
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_ENABLE_PIXEL_FEATURES := true
+TARGET_SUPPORTS_NEXT_GEN_ASSISTANT := true
+
+PRODUCT_NAME := rising_surya
 PRODUCT_DEVICE := surya
 PRODUCT_BRAND := POCO
 PRODUCT_MODEL := M2007J20CG
